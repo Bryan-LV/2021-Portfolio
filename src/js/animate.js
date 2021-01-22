a7f488e4b24473e133625c5155619b949b5a176c
Bryan Almaraz <bryan.almaraz96@gmail.com>
2021-01-22 11:17:42 -0600

    switch off animation

--- src/js/animate.js
+++ src/js/animate.js
@@ -1,6 +1,6 @@
-import { gsap } from "gsap";
-const heroSection = document.querySelector('#hero-section');
-const aboutSection = document.querySelector('#about-section');
+// import { gsap } from "gsap";
+// const heroSection = document.querySelector('#hero-section');
+// const aboutSection = document.querySelector('#about-section');
 
-gsap.fromTo(heroSection, { x: "-110vw" }, { x: 0, delay: 0.5, ease: "back" })
-gsap.fromTo(aboutSection, { x: "110vw" }, { x: 0, delay: 0.5, ease: "back" })
+// gsap.fromTo(heroSection, { x: "-110vw" }, { x: 0, delay: 0.5, ease: "back" })
+// gsap.fromTo(aboutSection, { x: "110vw" }, { x: 0, delay: 0.5, ease: "back" })
